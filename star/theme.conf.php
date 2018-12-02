62a37e20beee722d4801c85dc19578ae7c8961fd
Birhan Dursun <brhndursun@gmail.com>
2018-12-02 16:48:14 +0300

    Update theme.conf.php

--- star/theme.conf.php
+++ star/theme.conf.php
@@ -38,7 +38,7 @@ define('theme_name', "Star Theme");
 define('theme_author', "Distortion, Bootstrapdash");
 
 // Set the version of the theme here
-define('theme_version', "1.5.0");
+define('theme_version', "1.5.1");
 
 // Set the link of the theme here
 define('theme_link', "https://www.bootstrapdash.com/product/star-admin-free/");
