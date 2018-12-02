0bbe8d1c311e5a25aba4a5f33017f03240a9073e
Birhan Dursun <brhndursun@gmail.com>
2018-12-02 16:43:24 +0300

    New Calc Method

--- star/photoGET.php
+++ star/photoGET.php
@@ -1,30 +1,32 @@
 <?php
+
 if (!defined("IN_SB")) {
     echo "You should not be here. Only follow links!";
     die();
 }
-if(isset($_COOKIE["profilePhoto"]) || !empty($_COOKIE["profilePhoto"]))
-{
+if (isset($_COOKIE["profilePhoto"]) || !empty($_COOKIE["profilePhoto"])) {
 	echo $_COOKIE["profilePhoto"];
 }else{
-	
-	$steamId=$GLOBALS['userbank']->GetProperty("authid");
-	$steamId = str_replace('STEAM_', '' ,$steamId);
-	$parts = explode(':', $steamId);
-	$gameType = $parts[0];
-	$authServer = $parts[1];
-	$clientId = $parts[2];
-	$res = bcadd((bcadd('76561197960265728', $authServer)), (bcmul($clientId, '2')));
-	$cid = str_replace('.0000000000', '', $res);
-	
-    $endpoint = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" . STEAMAPIKEY . "&steamids=" . $res;
-    $data = json_decode(file_get_contents($endpoint), true);
-	if(isset($data['response']['players'][0]['personaname']))
-		$profilePicture = $data['response']['players'][0]['avatarmedium'];
-	else
+	$authid=$GLOBALS['userbank']->GetProperty("authid");
+	if (preg_match('/^STEAM_/',$authid)) {
+		$parts = explode(':', $authid);
+		$authServer = $parts[1];
+		$clientID = $parts[2];
+		$key = "76561197960265728";
+		$results = $GLOBALS['db']->Execute("Select @sum:=( ( $key + $authServer ) + ( $clientID * 2 ) ) as s64");
+		$res = $results->fields["s64"];
+
+	    $endpoint = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" . STEAMAPIKEY . "&steamids=" . $res;
+	    $data = json_decode(file_get_contents($endpoint), true);
+		if(isset($data['response']['players'][0]['avatarmedium']))
+			$profilePicture = $data['response']['players'][0]['avatarmedium'];
+		else
+			$profilePicture = "themes/star/images/faces/face".rand(1,27).".jpg";
+	}else{
 		$profilePicture = "themes/star/images/faces/face".rand(1,27).".jpg";
-	
+	}
 	echo $profilePicture;
 	setcookie("profilePhoto", $profilePicture, time()+9999999);
 }
+
 ?>
